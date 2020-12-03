7b45d17b7b04c4866333db4fbdc997e1cdfc22f8
Hippo <Hippah@protonmail.com>
2020-12-02 22:55:35 -0600

    Field annotations

---
+++ src/main/java/rip/hippo/parcel/packet/impl/in/PacketPlayInChat.java
@@ -0,0 +1,11 @@
+package rip.hippo.parcel.packet.impl.in;
+
+import rip.hippo.parcel.packet.Packet;
+
+/**
+ * @author Hippo
+ * @version 1.0.0, 12/2/20
+ * @since 1.0.0
+ */
+public abstract class PacketPlayInChat implements Packet {
+}
